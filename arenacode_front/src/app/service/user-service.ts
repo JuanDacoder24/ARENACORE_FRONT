7bfef59d914b298d483f6df658524901ced6dce1
COMPU-S033\daw2 <pedroarza.03@gmail.com>
2026-02-11 15:59:11 +0100

    mejora del user_service.ts

--- arenacode_front/src/app/service/user-service.ts
+++ arenacode_front/src/app/service/user-service.ts
@@ -2,18 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 
+export interface User {
+  username: string;
+  password: string;
+}
 
-@Injectable({
-  providedIn: 'root',
-})
+export interface LoginResponse {
+  token: string;
+}
+
+@Injectable({ providedIn: 'root' })
 export class UserService {
-  
-  private httpClient = inject(HttpClient)
-  private baseUrl: string = 'http://localhost:8080/login'
+  private httpClient = inject(HttpClient);
+  private baseUrl = 'http://localhost:8080';
 
-  login(user: User): Promise<any>{
-    return lastValueFrom(this.httpClient.post<any>(this.baseUrl + 'login', user))
+  login(user: User): Promise<LoginResponse> {
+    return lastValueFrom(
+      this.httpClient.post<LoginResponse>(`${this.baseUrl}/login`, user)
+    );
   }
-
-  
 }
