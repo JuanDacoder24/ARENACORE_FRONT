db7414d7ed6cc1faf62378c3909e5b9e6e72830b
COMPU-S033\daw2 <pedroarza.03@gmail.com>
2026-02-11 15:41:11 +0100

    Creacion de los dos services, arenacore_service no echo ya que necesito hacer antes otras cosas

--- arenacode_front/src/app/service/arenacore-service.ts
+++ arenacode_front/src/app/service/arenacore-service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArenacoreService {
   
+  private baseUrl: string = 'http://localhost:8080/arenacore'
+  private httpClient = inject(HttpClient)
+
+  constructor() { }
+
+  //traer todos los juegos 
+
+  //traer un juego por id
+
+  //añadir un juego
+
+  //editar un juego
+
+  //eliminar un juego
+
+  //insertar un juego 
+
+  //verificacion que se ah insertado el juego
+
+  
+
 }
--- arenacode_front/src/app/service/user-service.ts
+++ arenacode_front/src/app/service/user-service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserSerevice {
+export class UserService {
+  
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = 'http://localhost:8080/login'
+
+  login(user: User): Promise<any>{
+    return lastValueFrom(this.httpClient.post<any>(this.baseUrl + 'login', user))
+  }
+
   
 }
