736178c943b3161cb17bd7467dedeec77ed2c6bf
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2026-02-11 17:40:19 +0100

    interfaz de login casi terminada, faltaria la funcionalidad

--- arenacode_front/src/app/app.routes.ts
+++ arenacode_front/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { LanginPage } from './pages/langin-page/langin-page';
 import { Login } from './pages/login/login';
+import { PerfilUser } from './pages/perfil-user/perfil-user';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo:'landingPage'},
     {path: 'landingPage', component: LanginPage},
     {path: 'login', component: Login},
+    {
+        path: 'dashboard', component: Dashboard, canActivate:[authGuard], children:
+        [
+            {path:'', pathMatch:'full', redirectTo:'home'},
+            {path: 'perfilUser', component: PerfilUser},
+        ]
+    }
+    
 ];
