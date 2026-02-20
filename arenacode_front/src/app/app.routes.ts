9876d96bce0357e86dcd636c10fb52e6b89011ad
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2026-02-20 11:29:58 +0100

    error 404 terminado

--- arenacode_front/src/app/app.routes.ts
+++ arenacode_front/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Login } from './pages/login/login';
 import { PerfilUser } from './pages/perfil-user/perfil-user';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { authGuard } from './guards/auth-guard';
+import { Error404 } from './pages/error404/error404';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo:'landingPage'},
@@ -15,6 +16,8 @@ export const routes: Routes = [
             {path:'', pathMatch:'full', redirectTo:'home'},
             {path: 'perfilUser', component: PerfilUser},
         ]
-    }
+    },
+    {path: 'error404', component: Error404},
+    {path: '**', redirectTo: 'error404' }
     
 ];
