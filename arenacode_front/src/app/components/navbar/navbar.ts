cab04809a5814b43e781de9e2136d88fa558d44a
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2026-02-23 17:23:32 +0100

    no message

--- arenacode_front/src/app/components/navbar/navbar.ts
+++ arenacode_front/src/app/components/navbar/navbar.ts
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-navbar',
-  imports: [RouterLink],
-=======
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
->>>>>>> Service
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
-<<<<<<< HEAD
-  private router = inject(Router)
-
-  //moficar si es necesario 
-  logout(){
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('refreshToken')
-    this.router.navigate(['/home'])
-  }
-
-=======
->>>>>>> Service
 }
