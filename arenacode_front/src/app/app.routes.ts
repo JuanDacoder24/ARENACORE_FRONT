d01954ebdb0b06a6417e414d28a5904a975303bc
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2026-02-25 15:39:09 +0100

    arreglo de rutas

--- arenacode_front/src/app/app.routes.ts
+++ arenacode_front/src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { PerfilUser } from './pages/perfil-user/perfil-user';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { authGuard } from './guards/auth-guard';
 import { Error404 } from './pages/error404/error404';
+import { PageJuegos } from './pages/page-juegos/page-juegos';
+import { PageTorneos } from './pages/page-torneos/page-torneos';
+import { Faqs } from './pages/faqs/faqs';
+import { FormTorneo } from './pages/form-torneo/form-torneo';
+import { Info } from './pages/info/info';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo:'landingPage'},
@@ -15,6 +20,11 @@ export const routes: Routes = [
         [
             {path:'', pathMatch:'full', redirectTo:'home'},
             {path: 'perfilUser', component: PerfilUser},
+            {path: 'pageJuegos', component: PageJuegos},
+            {path: 'pageTorneos', component: PageTorneos},
+            {path: 'faqs', component: Faqs},
+            {path: 'formTorneo', component: FormTorneo},
+            {path: 'info', component: Info},
         ]
     },
     {path: 'error404', component: Error404},
