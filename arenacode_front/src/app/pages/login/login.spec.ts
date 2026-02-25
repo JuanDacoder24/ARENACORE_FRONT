2c6c2d7532c75c899666f354b9258baa5d64968c
COMPU-S033\daw2 <pedroarza.03@gmail.com>
2026-02-25 16:15:38 +0100

    no message

--- arenacode_front/src/app/pages/login/login.spec.ts
+++ arenacode_front/src/app/pages/login/login.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { Login } from './login';
+import { UserService } from '../../service/user-service';
+
+// minimal mock for login service
+const mockUserService = {
+  login: () => Promise.resolve({ success: true, data: { token: '', user: {} } })
+};
 
 describe('Login', () => {
   let component: Login;
@@ -8,9 +17,16 @@ describe('Login', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
-    })
-    .compileComponents();
+      imports: [
+        Login,
+        FormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: UserService, useValue: mockUserService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
